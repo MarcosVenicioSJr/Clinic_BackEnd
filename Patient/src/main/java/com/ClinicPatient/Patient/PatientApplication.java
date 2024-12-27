68eb0666b59a2a94a40e89cb4a4944deaee34d10
Marcos Venicio <marcos.venicio1230@hotmail.com>
2024-12-26 21:55:44 -0300

    Add config to PathVariable

--- Patient/src/main/java/com/ClinicPatient/Patient/PatientApplication.java
+++ Patient/src/main/java/com/ClinicPatient/Patient/PatientApplication.java
@@ -3,8 +3,10 @@ package com.ClinicPatient.Patient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ClinicPatient")
+@EnableJpaRepositories(basePackages = "com.ClinicPatient.repository")
 @EntityScan("com.ClinicPatient.models")
 public class PatientApplication {
 
--- Patient/src/main/java/com/ClinicPatient/controller/PatientController.java
+++ Patient/src/main/java/com/ClinicPatient/controller/PatientController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class PatientController {
   private PatientMapper mapper;
 
   @GetMapping("/{id}")
-  public ResponseEntity<GetResponse> getById(UUID id){
+  public ResponseEntity<GetResponse> getById(@PathVariable UUID id){
     GetResponse response = mapper.mapperGetByIdResponse(services.getById(id));
     return ResponseEntity.ok().body(response);
   }
