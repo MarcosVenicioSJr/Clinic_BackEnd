beaef7fa917909d5b6c56a47c363318d83ee8229
Marcos Venicio <marcos.venicio1230@hotmail.com>
2024-12-29 11:34:17 -0300

    Add docker

--- Patient/src/main/java/com/ClinicPatient/repository/AddressRepository.java
+++ Patient/src/main/java/com/ClinicPatient/repository/AddressRepository.java
@@ -1,13 +1,11 @@
 package com.ClinicPatient.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ClinicPatient.models.Address;
 
 @Repository
-public interface AddressRepository extends JpaRepository<Address, UUID>{
+public interface AddressRepository extends JpaRepository<Address, Integer>{
   
 }
